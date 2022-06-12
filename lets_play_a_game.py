da57628b336acc1b4def443e57ef13479d008156
Joshua Gierlich <joshuagierlich@Joshuas-MBP.lan>
2022-06-12 07:09:00 -0500

    adjusted the user input to be more like a dialogue

--- lets_play_a_game.py
+++ lets_play_a_game.py
@@ -1,25 +1,74 @@
+import random
+from pprint import pprint
+
+# Setting the scene
 print("Welcome to Fantasma's: Game of Horror\n brought to you by tweedledee\n")
+
 user_name = input("What's your name dear friend\n")
 
 print(f"Ah yes {user_name}, I thought I smelled fear.\n")
 
-print(f"Do you want to give a sarcastic or polite response?\n")
 
-sassy_response = input("s: sarcastic\np: polite\n")
+# To make this more game-like, we should be able to offer various options which we can program for
+feelings = ["insulted", "scared", "doesn't care" ]
 
-if sassy_response == "s":
-    print("I think that's your own stinky farts you are smelling.")
+
+# You know what a string is, pay attention to how one quote works vs, three quotes (demonstrated below)
+print("""
+      ********************
+      *      MENU        *
+      ********************
+      *** 1: insulted ****
+      *** 2: scared ******
+      *** 3: doesn't care*
+      ********************\n\n
+""")
+
+user_feels = input(f"{user_name} feels (choose a numbered option from above):")
+
+# This looks long and complicated, its not... we're taking the user input (their feelings) and just checking the number. If the number matches, we do everything within each block:
+if user_feels == "1":
+    #creating a bunch of different responses the user can choose from, using a dictionary.
+    options = {
+               1: "I think that's your own stinky farts you are smelling." ,
+               2: "Coming from a creature like you I'll take that as a compliment",
+               3: "Yeah, well your breath doesn't do the air any favors either"  
+    }
+
+    print("*********** Insult Options Are: *******************")
+    num = 1
+    pprint(options)
+
+    insult_Fantasma = input("What would you like to say to Fantasma? (input number here:)\n")
+    int_insult_Fantasma = int(insult_Fantasma)
+    print(f"\n{user_name} unflinchingly says to Fantasma: ", options[int_insult_Fantasma])
+
+elif user_feels == "2":
+
+    options = {
+               1: "Sweet Baby Jesus, uh uh.... wha... come back later" ,
+               2: "I choose life",
+               3: "Death isn't a good look for me man how'd the hell did I get in here anyway?!"
+    }
+    scared_Fantasma_input = input("What would you like to say to Fantasma? (input number here:\n")
 
 else:
-    print("*you nod politely*")
+# @Fatime !!!! I intentionally left an error in here, can you tell me what the error is, and why its an error?
+    options = {
+               1: "meh" ,
+               2: "Dude can we just get this useless dialogue over with already?",
+               3: "Hey, you're kinda scary, think you can scare my boss into giving me the day off?"
+    }
 
-print(f" Would you like to go on a journey with me? \n")
 
-journey_answer = input("y: yes\nn: no\n")
 
-if journey_answer == "y":
-    print("wonderful, glad to have you aboard!")
+#print(f" Would you like to go on a journey with me? \n")
 
-else:
-    print("fine, come back when you wanna talk")
-    exit()
+#journey_answer = input("y: yes\nn: no\n")
+
+#if journey_answer == "y":
+#    print("wonderful, glad to have you aboard!")
+
+#else:
+#    print("fine, come back when you wanna talk")
+#    exit()
